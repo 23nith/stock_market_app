71fbbe3532857bd8e936e8be9f90f8c9d803b834
23nith <zenithmanuel@gmail.com>
2022-05-16 06:03:36 +0800

    added json web token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable, :validatable, :confirmable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable, :confirmable
 
   enum role: [:user, :moderator, :admin]
   after_initialize :set_default_role, :if => :new_record?

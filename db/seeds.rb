7dae7bbf32ea7660bac016856f9f71990cab7619
23nith <zenithmanuel@gmail.com>
2022-05-20 20:54:05 +0800

    added database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(id: 0, email: "admin@email.com", password: "password", balance: 1000, role: 2, confirmed_at: DateTime.now)
